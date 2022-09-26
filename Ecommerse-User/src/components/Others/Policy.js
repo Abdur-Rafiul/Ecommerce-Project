f9770431556358dfbe14528268d3cbeb7057aff7
Rafiul-Islam <76603094+Abdur-Rafiul@users.noreply.github.com>
2022-09-26 23:43:47 +0600

    r

--- Ecommerse-User/src/components/Others/Policy.js
+++ Ecommerse-User/src/components/Others/Policy.js
@@ -1,42 +1,97 @@
 import React, {Component, Fragment} from 'react';
 import {Button, Col, Container, Form, Row} from "react-bootstrap";
-
+import axios from "axios";
+import ApiURL from "../../api/AppURL";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import ReactHtmlParser, {
+    processNodes,
+    convertNodeToElement,
+    htmlparser2,
+  }from "react-html-parser";
 class Policy extends Component {
+
+    constructor() {
+        super();
+        this.state = {
+            terms: "",
+          loaderDiv:"",
+          mainDiv:"d-none",
+        };
+      }
+      componentDidMount() {
+        if (sessionStorage.getItem("terms") == null) {
+          axios
+            .get(ApiURL.SendSiteInfo)
+            .then((response) => {
+              let StatusCode = response.status;
+              if (StatusCode == 200) {
+                let JSONDATA = response.data[0]["terms"];
+                this.setState({ terms: JSONDATA,loaderDiv:"d-none", mainDiv:" " });
+                sessionStorage.setItem("terms", JSONDATA);
+              }else{
+              
+               toast.error("Something Went Wrong! Refress The Page")
+            }
+            })
+            .catch((error) => {});
+        } else {
+          this.setState({ terms: sessionStorage.getItem("terms"),loaderDiv:"d-none", mainDiv:" "   });
+    
+        }
+      }
+
     render() {
         return (
             <Fragment>
-                <Container className="TopSection">
-                    <Row className="d-flex p-5  justify-content-center">
-                        <Col className="card mt-4" md={10} lg={10} sm={12} xs={12}>
-                            <div className="card-body">
-                           <p>Third parties that are engaged by us to provide verification services, such as conducting suitable money laundering, fraud and “Know Your Customer” checks on Buyers and Sellers. This includes your name, email address, company details, and contact information.
-                            We will only collect personal information from third parties where we have checked that these third parties either have your consent or are otherwise legally permitted or required to disclose your personal information to us.
-                           </p>
-
-                            <p>Third parties that are engaged by us to provide verification services, such as conducting suitable money laundering, fraud and “Know Your Customer” checks on Buyers and Sellers. This includes your name, email address, company details, and contact information.
-                                We will only collect personal information from third parties where we have checked that these third parties either have your consent or are otherwise legally permitted or required to disclose your personal information to us.
-                            </p>
-
-                            <p>Third parties that are engaged by us to provide verification services, such as conducting suitable money laundering, fraud and “Know Your Customer” checks on Buyers and Sellers. This includes your name, email address, company details, and contact information.
-                                We will only collect personal information from third parties where we have checked that these third parties either have your consent or are otherwise legally permitted or required to disclose your personal information to us.
-                            </p>
-
-                            <p>Third parties that are engaged by us to provide verification services, such as conducting suitable money laundering, fraud and “Know Your Customer” checks on Buyers and Sellers. This includes your name, email address, company details, and contact information.
-                                We will only collect personal information from third parties where we have checked that these third parties either have your consent or are otherwise legally permitted or required to disclose your personal information to us.
-                            </p>
-
-                            <p>Third parties that are engaged by us to provide verification services, such as conducting suitable money laundering, fraud and “Know Your Customer” checks on Buyers and Sellers. This includes your name, email address, company details, and contact information.
-                                We will only collect personal information from third parties where we have checked that these third parties either have your consent or are otherwise legally permitted or required to disclose your personal information to us.
-                            </p>
-
-                            <p>Third parties that are engaged by us to provide verification services, such as conducting suitable money laundering, fraud and “Know Your Customer” checks on Buyers and Sellers. This includes your name, email address, company details, and contact information.
-                                We will only collect personal information from third parties where we have checked that these third parties either have your consent or are otherwise legally permitted or required to disclose your personal information to us.
-                            </p>
-                            </div>
-                        </Col>
-                    </Row>
-                </Container>
-            </Fragment>
+            <Container className="TopSection">
+              <Row className="d-flex p-5  justify-content-center">
+                <Col className={this.state.loaderDiv} md={10} lg={10} sm={12} xs={12}>
+                  <div className="card-body">
+                    <div class="ph-item">
+                      <div class="ph-col-12">
+                      
+                        <div class="ph-row">
+                          <div class="ph-col-12"></div>
+                          <div class="ph-col-12"></div>
+                          <div class="ph-col-12"></div>
+                          <div class="ph-col-12"></div>
+                          <div class="ph-col-12"></div>
+                        </div>
+                      </div>
+    
+                      <div class="ph-col-12">
+                      
+                        <div class="ph-row">
+                          <div class="ph-col-12"></div>
+                          <div class="ph-col-12"></div>
+                          <div class="ph-col-12"></div>
+                          <div class="ph-col-12"></div>
+                          <div class="ph-col-12"></div>
+                        </div>
+                      </div>
+    
+                      <div class="ph-col-12">
+                      
+                        <div class="ph-row">
+                          <div class="ph-col-12"></div>
+                          <div class="ph-col-12"></div>
+                          <div class="ph-col-12"></div>
+                          <div class="ph-col-12"></div>
+                          <div class="ph-col-12"></div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </Col>
+                <Col className={this.state.mainDiv} md={10} lg={10} sm={12} xs={12}>
+                  <div className="card-body">
+                    {ReactHtmlParser(this.state.terms)}
+                  </div>
+                </Col>
+              </Row>
+            </Container>
+          </Fragment>
         );
     }
 }
